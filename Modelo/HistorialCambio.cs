ac567251e7f7a207d4b86f50bb1a0dbd56c0af7c
Jack <132464970+jackhfernandez@users.noreply.github.com>
2026-08-10 01:01:08 -0500

    Agregando modelos: tarea 28

---
+++ Modelo/HistorialCambio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo
+{
+    public class HistorialCambio
+    {
+        public int HistorialId { get; set; }
+        public string Entidad { get; set; }
+        public int EntidadId { get; set; }
+        public string CampoModificado { get; set; }
+        public string ValorAnterior { get; set; }
+        public string ValorNuevo { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime FechaModificacion { get; set; }
+
+        public HistorialCambio()
+        {
+            FechaModificacion = DateTime.Now;
+        }
+
+        public HistorialCambio(string entidad, int entidadId, string campoModificado, string valorAnterior, string valorNuevo, int usuarioId)
+            : this()
+        {
+            Entidad = entidad;
+            EntidadId = entidadId;
+            CampoModificado = campoModificado;
+            ValorAnterior = valorAnterior;
+            ValorNuevo = valorNuevo;
+            UsuarioId = usuarioId;
+        }
+    }
+}
---
+++ Modelo/Notificacion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo
+{
+    public class Notificacion
+    {
+        public int NotificacionId { get; set; }
+        public int UsuarioId { get; set; }
+        public int? UserStoryId { get; set; }
+        public string Titulo { get; set; }
+        public string Mensaje { get; set; }
+        public bool Leido { get; set; }
+        public DateTime FechaNotificacion { get; set; }
+
+        public Notificacion()
+        {
+            Leido = false;
+            FechaNotificacion = DateTime.Now;
+        }
+
+        public Notificacion(int usuarioId, string titulo, string mensaje, int? userStoryId = null)
+            : this()
+        {
+            UsuarioId = usuarioId;
+            Titulo = titulo;
+            Mensaje = mensaje;
+            UserStoryId = userStoryId;
+        }
+    }
+}
